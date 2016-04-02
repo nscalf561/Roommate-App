16fee110c940921862c7bc8f0544c990f4c72738
nscalf561 <scalf.nicholas@gmail.com>
2016-04-02 13:28:00 -0700

    seperates frontend and backendfolder structure

--- backend/models/house.js
+++ backend/models/house.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose'),
 		Schema = mongoose.Schema;
 
 var houseSchema = new Schema ({
-	// TODO
+	id: String,
+	name: String, 
+	address: String
 });
 
 var House = mongoose.model('House', houseSchema);
