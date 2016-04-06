f244d501b5f8395d3ceae21c4c9b43ab33bc6461
chencaleb <clb_chen@berkeley.edu>
2016-04-05 19:07:00 -0700

    chorecontroller changes

--- frontend/www/controllers/choreController.js
+++ frontend/www/controllers/choreController.js
@@ -21,8 +21,6 @@ angular.module('chore.controller', ['ionic'])
   		});
   }
 
-
-
 	// New Chore Modal Functions
   // Creates and loads the new chore modal
   $ionicModal.fromTemplateUrl('new-chore.html', function(modal) {
